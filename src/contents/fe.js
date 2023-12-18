8c110832780a9a0fddd4d1f5a33da0453c88b69d
muhamadadam2001 <61117916+admnsonn@users.noreply.github.com>
2023-12-18 23:09:53 +0700

    update project

--- src/contents/fe.js
+++ src/contents/fe.js
@@ -1,12 +1,22 @@
 export default [
 	{
-		thumb: 'https://drive.google.com/uc?id=1P67hMUjs1a5z4MCLTxjkMIK1a956qvfB&export=download',
+		thumb: 'https://drive.google.com/uc?id=1Q9mHyuWey9TKd_dsdT_iI3KcR_LCQw7C&export=download',
 		title: 'Administration Management Cafeasy',
 		tags: [
 			'web-app', 'reactjs', 'nodejs', 'bootstrap'
 		],
 		links: {
 			github: 'https://github.com/Cafeasy/cafeasy-admin',
+		}
+	},
+	{
+		thumb: 'https://drive.google.com/uc?id=1OpLLQZJXRwO4kGwFKEUJ9wtLK96UjeMh&export=download',
+		title: 'Village Service App Application: Bahomoleo',
+		tags: [
+			'web-app', 'vuejs', 'nodejs', 'bootstrap'
+		],
+		links: {
+			github: 'https://github.com/admnsonn/morowali-project',
 		}
 	},
 ]
