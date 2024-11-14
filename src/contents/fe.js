94a041f6b27cb072831eacca59802964a029dadd
muhamadadam2001 <61117916+admnsonn@users.noreply.github.com>
2024-11-15 01:51:48 +0700

    pembaharuan

--- src/contents/fe.js
+++ src/contents/fe.js
@@ -1,22 +1,26 @@
 export default [
-	{
-		thumb: 'project/cafeasy-fe.png',
-		title: 'Administration Management Cafeasy',
-		tags: [
-			'web-app', 'reactjs', 'nodejs', 'bootstrap'
-		],
-		links: {
-			github: 'https://github.com/Cafeasy/cafeasy-admin',
-		}
-	},
-	{
-		thumb: 'project/bahomoleo-fe.png',
-		title: 'Village Service App Application: Bahomoleo',
-		tags: [
-			'web-app', 'vuejs', 'nodejs', 'bootstrap'
-		],
-		links: {
-			github: 'https://github.com/admnsonn/morowali-project',
-		}
-	},
-]
+  {
+    thumb: "project/cafeasy-fe.png",
+    title: "Administration Management Cafeasy",
+    tags: ["web-app", "reactjs", "nodejs", "bootstrap"],
+    links: {
+      github: "https://github.com/Cafeasy/cafeasy-admin",
+    },
+  },
+  {
+    thumb: "project/bahomoleo-fe.png",
+    title: "Village Service App Application: Bahomoleo",
+    tags: ["web-app", "vuejs", "nodejs", "bootstrap"],
+    links: {
+      github: "https://github.com/admnsonn/morowali-project",
+    },
+  },
+  {
+    thumb: "project/otono-fe.png",
+    title: "Otonometer.com",
+    tags: ["web-app", "reactjs", "tailwindcss"],
+    links: {
+      github: "https://github.com/admnsonn/Otonometer",
+    },
+  },
+];
--- src/contents/ui.js
+++ src/contents/ui.js
@@ -1,52 +1,55 @@
 export default [
-	{
-		thumb: 'project/baznas-ui.png',
-		title: 'Baznas V2',
-		tags: [
-			'redesign-page', 'figma', 'user-research', 'prototype'
-		],
-		links: {
-			figma: 'https://www.figma.com/proto/bSNKxYYiQqgM7en3epVMDK/Baznas-v2?page-id=2%3A4&type=design&node-id=305-14199&viewport=1785%2C1267%2C0.08&t=XTOn42uAF7Wi65ju-1&scaling=scale-down&starting-point-node-id=305%3A14199&mode=design',
-		}
-	},
-	{
-		thumb: 'project/cafeasy-ui.png',
-		title: 'Cafeasy',
-		tags: [
-			'web-design', 'figma', 'user-research', 'high-fidelity'
-		],
-		links: {
-			figma: 'https://www.figma.com/proto/YpdEhU7lSIGtWoFiIsstNC/Prototype?page-id=0%3A1&type=design&node-id=27-329&viewport=460%2C245%2C0.12&t=FR4m3DkueCbP2pGx-1&scaling=scale-down&starting-point-node-id=1%3A3&mode=design'
-		}
-	},
-	{
-		thumb: 'project/cafeasy-admin-ui.png',
-		title: 'Administration Management Cafeasy',
-		tags: [
-			'web-design', 'figma', 'user-research', 'high-fidelity'
-		],
-		links: {
-			figma: 'https://www.figma.com/proto/YpdEhU7lSIGtWoFiIsstNC/Prototype?page-id=1839%3A1818&type=design&node-id=1839-4274&viewport=-505%2C-123%2C0.22&t=cjEQbzssg4o3ruCf-1&scaling=scale-down&starting-point-node-id=1839%3A4274&mode=design'
-		}
-	},
-	{
-		thumb: 'project/self-ui.png',
-		title: 'Self Safety Report',
-		tags: [
-			'app-design', 'figma', 'user-research', 'high-fidelity'
-		],
-		links: {
-			figma: 'https://www.figma.com/proto/lIugINiCQS1hdFMCWeNbZi/Project-Prototype?page-id=0%3A1&type=design&node-id=425-2746&viewport=99%2C350%2C0.13&t=A0f1ZVFeOLxLJqzX-1&scaling=scale-down&starting-point-node-id=425%3A2746&show-proto-sidebar=1&mode=design'
-		}
-	},
-       {
-		thumb: 'project/bahomoleo-ui.png',
-		title: 'Bahomoleo Village',
-		tags: [
-			'web-design', 'figma', 'user-research', 'high-fidelity'
-		],
-		links: {
-			figma: '/'
-		}
-	},
-]
+  {
+    thumb: "project/baznas-ui.png",
+    title: "Baznas V2",
+    tags: ["redesign-page", "figma", "user-research", "prototype"],
+    links: {
+      figma:
+        "https://www.figma.com/proto/bSNKxYYiQqgM7en3epVMDK/Baznas-v2?page-id=2%3A4&type=design&node-id=305-14199&viewport=1785%2C1267%2C0.08&t=XTOn42uAF7Wi65ju-1&scaling=scale-down&starting-point-node-id=305%3A14199&mode=design",
+    },
+  },
+  {
+    thumb: "project/cafeasy-ui.png",
+    title: "Cafeasy",
+    tags: ["web-design", "figma", "user-research", "high-fidelity"],
+    links: {
+      figma:
+        "https://www.figma.com/proto/YpdEhU7lSIGtWoFiIsstNC/Prototype?page-id=0%3A1&type=design&node-id=27-329&viewport=460%2C245%2C0.12&t=FR4m3DkueCbP2pGx-1&scaling=scale-down&starting-point-node-id=1%3A3&mode=design",
+    },
+  },
+  {
+    thumb: "project/cafeasy-admin-ui.png",
+    title: "Administration Management Cafeasy",
+    tags: ["web-design", "figma", "user-research", "high-fidelity"],
+    links: {
+      figma:
+        "https://www.figma.com/proto/YpdEhU7lSIGtWoFiIsstNC/Prototype?page-id=1839%3A1818&type=design&node-id=1839-4274&viewport=-505%2C-123%2C0.22&t=cjEQbzssg4o3ruCf-1&scaling=scale-down&starting-point-node-id=1839%3A4274&mode=design",
+    },
+  },
+  {
+    thumb: "project/self-ui.png",
+    title: "Self Safety Report",
+    tags: ["app-design", "figma", "user-research", "high-fidelity"],
+    links: {
+      figma:
+        "https://www.figma.com/proto/lIugINiCQS1hdFMCWeNbZi/Project-Prototype?page-id=0%3A1&type=design&node-id=425-2746&viewport=99%2C350%2C0.13&t=A0f1ZVFeOLxLJqzX-1&scaling=scale-down&starting-point-node-id=425%3A2746&show-proto-sidebar=1&mode=design",
+    },
+  },
+  {
+    thumb: "project/bahomoleo-ui.png",
+    title: "Bahomoleo Village",
+    tags: ["web-design", "figma", "user-research", "high-fidelity"],
+    links: {
+      figma: "/",
+    },
+  },
+  {
+    thumb: "project/otono-ui.png",
+    title: "Otonometer.com",
+    tags: ["web-design", "figma", "user-interface", "high-fidelity"],
+    links: {
+      figma:
+        "https://www.figma.com/proto/cDE4MWpaZPFl8XNQaaejdV/OTONOMETER_NR_WEBSITE?page-id=0%3A1&node-id=326-8836&node-type=canvas&viewport=-30%2C304%2C0.03&t=4Uas6iVMJXEWvyDj-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=244%3A9552",
+    },
+  },
+];
